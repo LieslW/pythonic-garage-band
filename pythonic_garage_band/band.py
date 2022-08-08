75118addadf7cbf3f4e5abcc2603180534f2277c
Liesl White <lieslfwhite@gmail.com>
2022-08-07 19:34:07 -0700

    band class built, not yet tested

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,7 +1,20 @@
 class Band():
-    # lists (?)
-    # __init__ method
-    # methods
+    instances = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+
+    def play_solos(self):
+        solos = []
+        for players in self.members:
+            solos.append(players.play_solo())
+        return solos
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 class Musician():
 
