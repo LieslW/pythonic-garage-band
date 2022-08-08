083a651cb1533253ccf6493487e679bb0214fab8
Liesl White <lieslfwhite@gmail.com>
2022-08-07 19:50:15 -0700

    functions built, organized, and tests (finally) passing

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -16,12 +16,55 @@ class Band():
     def to_list(cls):
         return cls.instances
 
+
 class Musician(Band):
     def __init__(self, name):
         self.name = name
 
+
 class Guitarist(Musician):
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "guitar"
+
+    @staticmethod
+    def play_solo():
+        return "face melting guitar solo"
+
 
 class Bassist(Musician):
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "bass"
+
+    @staticmethod
+    def play_solo():
+        return "bom bom buh bom"
+
 
 class Drummer(Musician):
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "drums"
+
+    @staticmethod
+    def play_solo():
+        return "rattle boom crash"
